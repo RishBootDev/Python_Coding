3bd23680d0ab74e0052fb60b40f69a2d4d96ec42
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-21 11:05:09 +0530

    python

---
+++ Basics/string_split.py
@@ -0,0 +1,10 @@
+
+st = "Hello , My name is Rishabh"
+print(st)
+
+temp = st.split('e')
+print(temp)
+
+print(type(temp))
+# the split method returns the list object and we can pass any regex inside the split method
+
