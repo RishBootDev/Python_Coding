93aa4e3ba77858911374e0cac388edc69b2440e6
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-21 11:14:46 +0530

    updated

---
+++ Basics/tuple_sorting.py
@@ -0,0 +1,9 @@
+# I am given a tuple and asked to sort this tuple
+
+tup = (45, 23, 67, 1, 99, 5)
+
+list = sorted(tup)
+print(list)
+
+# the tuples are immutable and we cannot modify the data inside them 
+# so we use the sorted method that returns the sorted list form
